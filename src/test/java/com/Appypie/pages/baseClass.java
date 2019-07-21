d80f6dfac15c7b5de18b2c8c79d6a59946403fbf
Lifesorted <mailmeshailendrakumar@gmail.com>
2019-07-21 01:27:25 -0700

    add config data provider in baseClass

--- src/test/java/com/Appypie/pages/baseClass.java
+++ src/test/java/com/Appypie/pages/baseClass.java
@@ -5,17 +5,28 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import com.Appypie.Utility.browserFactory;
+import com.Appypie.Utility.configDataProvider;
+import com.Appypie.Utility.excelDataProvider;
 
 public class baseClass {
 
 	public WebDriver driver;
 	
-		
+	public excelDataProvider excel;
+	public configDataProvider configdp;
+	
+	@BeforeSuite
+	public void setupSuite() {
+		excel=new excelDataProvider();
+		configdp=new configDataProvider();
+	}
+	
 	@BeforeMethod
 	public void launchBrowser() {
-		driver=browserFactory.startApplication(driver, "chrome", "Https://www.appypie.com");
+		driver=browserFactory.startApplication(driver, configdp.getBrowser(), configdp.getUrl());
 	}
 	
 	@AfterMethod
